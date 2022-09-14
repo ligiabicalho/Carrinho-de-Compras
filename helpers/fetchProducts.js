162c18bdd94d0b418064ac18115edf072739a28f
Lígia Bicalho <ligiabicalho@gmail.com>
2022-09-14 16:42:53 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,12 @@
-const fetchProducts = (product) => {
-  const url = `api.mercadolibre.com/sites/MLB/search?q=${product}`;
-  fetch(url);
+const fetchProducts = async (product) => {
+  try { 
+    const urlEndpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+    const resp = await fetch(urlEndpoint);
+    const result = await resp.json();
+    return result;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -9,20 +9,22 @@ describe('1 - Teste a função fetchProducts', () => {
     expect(typeof fetchProducts === 'function').toBeTruthy();
   });
 
-  it('Execute a função fetchProducts com o argumento computador e teste se fetch foi chamada', () => {
-    expect(fetchProducts('computador')).toHaveBeenCalled();
+  it('Execute a função fetchProducts com o argumento computador e teste se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  // it('Teste se fetchProducts é uma função', () => {
-  //   expect().toBe();
-  // });
+  it('Teste se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint correspondente', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
 
-  // it('Teste se fetchProducts é uma função', () => {
-  //   expect().toBe();
-  // });
+  it('Teste se o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
 
-  // it('Teste se fetchProducts é uma função', () => {
-  //   expect().toBe();
-  // });
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    expect(await fetchProducts()).toBe('You must provide an url');
+  });
 
 });
